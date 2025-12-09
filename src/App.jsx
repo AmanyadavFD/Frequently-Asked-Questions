83d6254605787c645f7426840df318d80cc327c4
amDeq <amyadav@deqode.com>
2025-12-10 05:06:09 +0530

    build faq asked question

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import FaqCom from "./componets/FaqCom";
+
+function App() {
+  return (
+    <>
+      <h1>Frequently Asked Questions</h1>
+      <FaqCom />
+    </>
+  );
+}
+
+export default App;
